ae7be64d8839dfc32f967fa81223bf923eabf211
Tom Kraina <tomas.kraina@gmail.com>
2018-09-20 12:28:52 +0200

    Initial Commit

---
+++ KomootChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KomootChallenge
+//
+//  Created by Tom Kraina on 20/09/2018.
+//  Copyright © 2018 Tom Kraina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
